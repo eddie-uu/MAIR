8ea3b83ebf2e02e76b6705bc7b16b9f5cda39d31
Eddie <c.l.wong@students.uu.nl>
2020-09-10 12:12:31 +0200

    Added comments for more clarity

--- decision_tree.py
+++ decision_tree.py
@@ -5,82 +5,88 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from extract import extract_data
 import numpy as np
-import graphviz
 
-# Parsed data from "dialog_acts.dat", with 85% training data
-data              = extract_data("dialog_acts.dat", 0.85) # 85
+def decisionTree():
+    # Parsed data from "dialog_acts.dat", with 85% training data
+    data              = extract_data("dialog_acts.dat", 0.85)
 
-# Array of values from each data key
-dialog_acts_train = data["dialog_acts_train"]
-sentences_train   = data["sentences_train"]
-dialog_acts_test  = data["dialog_acts_test"]
-sentences_test    = data["sentences_test"]
-max_len_train = len(max(sentences_train,key=len))
-max_len_test = len(max(sentences_test,key=len))
-max_len = max_len_train
+    # Array of values from each data key
+    dialog_acts_train = data["dialog_acts_train"]
+    sentences_train   = data["sentences_train"]
+    dialog_acts_test  = data["dialog_acts_test"]
+    sentences_test    = data["sentences_test"]
 
-if (max_len_test > max_len_train):
-    max_len = max_len_test
+    # Get maximum length of sentence
+    max_len_train = len(max(sentences_train,key=len))
+    max_len_test = len(max(sentences_test,key=len))
+    max_len = max_len_train
 
-for sentence in sentences_train:
-    for n in range(len(sentence), max_len, 1):
-        sentence.insert(n, '')
+    if (max_len_test > max_len_train):
+        max_len = max_len_test
 
-for sentence in sentences_test:
-    for n in range(len(sentence), max_len, 1):
-        sentence.insert(n, '')
-    
-oneHotEncoder = OneHotEncoder(sparse=False, handle_unknown='ignore')
-oneHotEncoder.fit(sentences_train) # sentences_train
-oneHotLabels = oneHotEncoder.transform(sentences_train) # sentences_train
+    # Make all sentences equal in length to parse with OneHotEncoder to binary
+    for sentence in sentences_train:
+        for n in range(len(sentence), max_len, 1):
+            sentence.insert(n, '')
 
-decisionTreeClassifier = tree.DecisionTreeClassifier()
-decisionTreeClassifier = decisionTreeClassifier.fit(oneHotLabels, dialog_acts_train) #dialog_acts_train
+    for sentence in sentences_test:
+        for n in range(len(sentence), max_len, 1):
+            sentence.insert(n, '')
 
-while True:
-    print("Message: ")
-    choice = input("> ")
-    
-    if (choice == 'test'):
-        tested = 0
-        correct = 0
-        for n in range(0, len(dialog_acts_test), 1):
-            dialog = dialog_acts_test[n]
-            sentence = sentences_test[n]
+    # Convert string lists to binary lists, since SciKit decision tree does not support string data        
+    oneHotEncoder = OneHotEncoder(sparse=False, handle_unknown='ignore')
+    oneHotEncoder.fit(sentences_train)
+    oneHotLabels = oneHotEncoder.transform(sentences_train)
 
-            prediction = oneHotEncoder.transform([sentence])
-            answer = decisionTreeClassifier.predict(prediction)
-            
-            if (answer[0] == dialog):
-                correct = correct + 1
-            tested = tested + 1    
+    # Create the decision tree
+    decisionTreeClassifier = tree.DecisionTreeClassifier()
+    decisionTreeClassifier = decisionTreeClassifier.fit(oneHotLabels, dialog_acts_train)
+
+    # Print decision tree
+    # r = export_text(decisionTreeClassifier)
+    # print(r)
+
+    # Console writeline
+    while True:
+        print("Message: ")
+        choice = input("> ")
         
-        print("Tested sentences: " + str(tested))
-        print("Correctly classified: " + str(correct))
-        print("Finished testing, results: " + str(float(100 / tested * correct)) + "% correct")
-        break
-    else:    
-        choice = choice.lower().split(' ')
+        # Write 'test' in console to start testing sequence
+        if (choice == 'test'):
+            tested = 0
+            correct = 0
+            for n in range(0, len(dialog_acts_test), 1):
+                dialog = dialog_acts_test[n]
+                sentence = sentences_test[n]
 
-        if (len(choice) < max_len):
-            for n in range(len(choice), max_len, 1):
-                choice.insert(n, '')
+                prediction = oneHotEncoder.transform([sentence])
+                answer = decisionTreeClassifier.predict(prediction)
+                
+                if (answer[0] == dialog):
+                    correct = correct + 1
+                tested = tested + 1    
+            
+            print("Tested sentences: " + str(tested))
+            print("Correctly classified: " + str(correct))
+            print("Finished testing, results: " + str(float(100 / tested * correct)) + "% correct")
+            break
         else:
-            choice = choice[0:max_len]
+            # Label user input
+            choice = choice.lower().split(' ')
 
-        prediction = oneHotEncoder.transform([choice])
-        answer = decisionTreeClassifier.predict(prediction)
+            if (len(choice) < max_len):
+                for n in range(len(choice), max_len, 1):
+                    choice.insert(n, '')
+            else:
+                choice = choice[0:max_len]
 
-        print(answer)
+            prediction = oneHotEncoder.transform([choice])
+            answer = decisionTreeClassifier.predict(prediction)
 
-        if answer[0] == 'bye':
-            break  
+            print(answer)
 
-# r = export_text(decisionTreeClassifier)
-# print(r)
+            # Quit program if the answer given is labeled 'bye'
+            if answer[0] == 'bye':
+                break
 
-"""
-dot_data = tree.export_graphviz(decisionTreeClassifier, out_file=None)
-graph = graphviz.Source(dot_data)
-graph.render("Chatbot")
-"""
+decisionTree()
