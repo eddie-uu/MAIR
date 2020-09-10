0565ebfa237d0fd4eea5dc2b618c62194169e313
Eddie <c.l.wong@students.uu.nl>
2020-09-10 11:50:31 +0200

    testing sentences added

--- decision_tree.py
+++ decision_tree.py
@@ -13,10 +13,8 @@ data              = extract_data("dialog_acts.dat", 0.85) # 85
 # Array of values from each data key
 dialog_acts_train = data["dialog_acts_train"]
 sentences_train   = data["sentences_train"]
-# sentences_train   = [[" ".join(sentence)] for sentence in sentences_train]
 dialog_acts_test  = data["dialog_acts_test"]
 sentences_test    = data["sentences_test"]
-# sentences_test   = [[" ".join(sentence)] for sentence in sentences_test]
 max_len_train = len(max(sentences_train,key=len))
 max_len_test = len(max(sentences_test,key=len))
 max_len = max_len_train
@@ -39,25 +37,44 @@ oneHotLabels = oneHotEncoder.transform(sentences_train) # sentences_train
 decisionTreeClassifier = tree.DecisionTreeClassifier()
 decisionTreeClassifier = decisionTreeClassifier.fit(oneHotLabels, dialog_acts_train) #dialog_acts_train
 
-
 while True:
     print("Message: ")
     choice = input("> ")
-    choice = choice.lower().split(' ')
+    
+    if (choice == 'test'):
+        tested = 0
+        correct = 0
+        for n in range(0, len(dialog_acts_test), 1):
+            dialog = dialog_acts_test[n]
+            sentence = sentences_test[n]
 
-    if (len(choice) < max_len):
-        for n in range(len(choice), max_len, 1):
-            choice.insert(n, '')
-    else:
-        choice = choice[0:max_len]
+            prediction = oneHotEncoder.transform([sentence])
+            answer = decisionTreeClassifier.predict(prediction)
+            
+            if (answer[0] == dialog):
+                correct = correct + 1
+            tested = tested + 1    
+        
+        print("Tested sentences: " + str(tested))
+        print("Correctly classified: " + str(correct))
+        print("Finished testing, results: " + str(float(100 / tested * correct)) + "% correct")
+        break
+    else:    
+        choice = choice.lower().split(' ')
 
-    prediction = oneHotEncoder.transform([choice])
-    answer = decisionTreeClassifier.predict(prediction)
+        if (len(choice) < max_len):
+            for n in range(len(choice), max_len, 1):
+                choice.insert(n, '')
+        else:
+            choice = choice[0:max_len]
 
-    print(answer)
+        prediction = oneHotEncoder.transform([choice])
+        answer = decisionTreeClassifier.predict(prediction)
 
-    if answer[0] == 'bye':
-        break  
+        print(answer)
+
+        if answer[0] == 'bye':
+            break  
 
 # r = export_text(decisionTreeClassifier)
 # print(r)
