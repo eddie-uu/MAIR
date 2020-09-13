3840086c7abf0373b75d4ad9fd734cfd5a18c391
Eddie <c.l.wong@students.uu.nl>
2020-09-13 17:10:57 +0200

    merge Bence

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ def main():
     print("Type one of the following numbers to execute the command: ")
     print("1. test baseline")
     print("2. test decision tree")
-    print("3. baseline")
-    print("4. decision tree")
+    print("3. test neural network")
+    print("4. baseline")
+    print("5. decision tree")
+    print("6. neural network")
     print("")
 
     userInput = input("> ")
@@ -21,9 +23,13 @@ def main():
     elif userInput == '2':
         decisionTree('test')
     elif userInput == '3':
-        classify_user_input()
+        print('neural network test')
     elif userInput == '4':
+        classify_user_input()
+    elif userInput == '5':
         decisionTree('')
+    elif userInput == '6':
+        print('neural network')
 
 def classify_user_input():
     data = extract_data("dialog_acts.dat")
@@ -37,3 +43,5 @@ def classify_user_input():
     if (len(sentence) > 0):
         print('Majority classification is: '  + majority_baseline(data)[0])
         print('Rule based classification is: '  + rule_based_baseline(sentence)[0])
+
+main()
