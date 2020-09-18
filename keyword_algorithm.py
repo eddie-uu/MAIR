cb1157ac3773330e25ac9e713f8b3399c75b8d59
Eddie <c.l.wong@students.uu.nl>
2020-09-18 12:49:34 +0200

    setup keyword algorithm

---
+++ keyword_algorithm.py
@@ -0,0 +1,60 @@
+import re
+
+def keywordAlgorithm(text, mode):
+    text = text.lower()
+    dontCarePatterns = ['any', 'doesnt matter', 'dont care']
+    # If currently asked about food
+    if mode == 'food':
+        filterPatterns = ['cheap', 'moderately', 'expensive']
+        foodPatterns = [{'pattern': '(.+?) (food)', 'group': 1},
+                        {'pattern': '(serving|serves|serve) (.*)', 'group': 2},
+                        {'pattern': '(with|about) (.*)', 'group': 2},
+                        {'pattern': '(for|a) (.*)', 'group': 2},
+                        {'pattern': '(.+?) (restaurant)', 'group': 1}]
+
+        for footPattern in foodPatterns:
+            match = re.search(footPattern['pattern'], text)
+            if match:
+                text = match.group(footPattern['group'])
+
+        for dontCarePattern in dontCarePatterns:
+            match = re.search(dontCarePattern, text)
+            if match:
+                text = 'any'
+        return text
+
+    elif mode == 'price':
+        pricePatterns = ['cheap', 'moderate', 'expensive', 'moderately']
+        print(str(pricePatterns))
+    elif mode == 'location':
+        areaPatterns = ['centre', 'north', 'south', 'east', 'west']
+        print(text)
+    
+text = 'im looking for a moderately priced restaurant that serves'
+texts = ['I\'m looking for world food',
+         'I want a restaurant that serves world food',
+         'I want a restaurant serving Swedish food',
+         'I\'m looking for a restaurant in the center',
+         'I would like a cheap restaurant in the west part of town',
+         'I\'m looking for a moderately priced restaurant in the west part of town',
+         'I\'m looking for a restaurant in any area that serves Tuscan food',
+         'Can I have an expensive restaurant',
+         'I\'m looking for an expensive restaurant and it should serve international food',
+         'I need a Cuban restaurant that is moderately priced',
+         'I\'m looking for a moderately priced restaurant with Catalan food',
+         'What is a cheap restaurant in the south part of town',
+         'What about Chinese food',
+         'I wanna find a cheap restaurant',
+         'I\'m looking for Persian food please',
+         'Find a Cuban restaurant in the center',
+         'is there a moderately priced restaurant that serves british food',
+         'moderately priced restaurant in the south part of town',
+         'a cheap mexican restaurant',
+         'It doesnt matter']
+mode = 'food'
+
+integer = 1
+for message in texts:
+    found = keywordAlgorithm(message, mode)
+    print(str(integer) + " " + found)
+    integer += 1
