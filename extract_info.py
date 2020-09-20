c7d51b57be1581441e68584d92c5abb80d34aedf
Bence <bencetijssen@gmail.com>
2020-09-20 18:54:46 +0200

    Prettier restaurant info.

--- extract_info.py
+++ extract_info.py
@@ -3,6 +3,7 @@ import Levenshtein
 from nltk.corpus import wordnet 
 from collections import defaultdict
 import re
+import numpy as np
 
 def extract_info(csv_file, request):
     data = pd.read_csv(csv_file)
@@ -10,12 +11,34 @@ def extract_info(csv_file, request):
     for pref_type in request:
         data = modify_data(data, pref_type, request[pref_type])
     data = data.sample(frac=1)
-    data["restaurantname"] = data["restaurantname"].apply(lambda m: " ".join([word.capitalize() for word in m.split(" ")]))
-    # data["phone"] = data["phone"].apply(lambda m: ))
+    capt = lambda m: " ".join([word.capitalize() for word in m.split(" ")])
+    data["restaurantname"] = data["restaurantname"].apply(capt)
+    data["phone"] = data["phone"].apply(fix_phone_number)
+    data["addr"] = data["addr"].apply(add_comma)
+    data["addr"] = data["addr"].apply(lambda m: capt(m) if isinstance(m, str) else np.nan)
     return data
 
-# def fix_phone_number(phone_number):
-#     if re.match("^[0-9 ]+$", myString):
+def add_comma(address):
+    if isinstance(address, float):
+        return np.nan
+    street_types = ["road", "street", "avenue", "lane", "way"]
+    for street_type in street_types:
+        print(address[:-len(street_type)])
+        print(street_type)
+        if street_type in address and address[-len(street_type):] != street_type:
+            comma_index = address.rfind(street_type) + len(street_type)
+            address = address[:comma_index] + ',' + address[comma_index:]
+            break
+    return address
+
+def fix_phone_number(phone_number):
+    if isinstance(phone_number, str) and re.match(r"^[0-9 \-]+$", phone_number):
+        phone_number = re.sub(r"\D+", '', phone_number)
+        if len(phone_number) == 11:
+            phone_number = phone_number[:5] + ' ' + phone_number[5:]
+            phone_number = phone_number[:9] + ' ' + phone_number[9:]
+        return phone_number
+    return np.nan
 
 def modify_data(data, pref_type, user_input):
     user_input = user_input.lower()
@@ -50,4 +73,4 @@ def levenshtein_or_synonym(word, options, threshold):
         return best
 
 if __name__ == "__main__":
-    print(extract_info("restaurant_info.csv", {"pricerange":"cheap", "area":"south", "food":"dontcare"}))
+    print(extract_info("restaurant_info.csv", {"pricerange":"expensive", "area":"centre", "food":"dontcare"}))
